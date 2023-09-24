a6c5d285c20b1127414003ff47dfac7258f947b0
Kevin Cho <126820348+kcho760@users.noreply.github.com>
2023-09-24 15:54:10 -0400

    Time: 57 ms (25.02%), Space: 41.8 MB (61.3%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.js
@@ -4,13 +4,12 @@
  */
 var numberOfSteps = function(num) {
     let step = 0;
-    let current = num;
-    while(current !== 0){
-        if(current%2 === 0){
-            current /= 2;
+    while(num !== 0){
+        if(num%2 === 0){
+            num /= 2;
             step += 1;
         }else{
-            current -= 1;
+            num -= 1;
             step += 1;
         }
     }
