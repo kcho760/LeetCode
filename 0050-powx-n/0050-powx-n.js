f7bed493ecc8d509da8122996140073c466a5055
Kevin Cho <126820348+kcho760@users.noreply.github.com>
2023-10-20 17:38:49 -0400

    Time: 54 ms (56.08%), Space: 42.1 MB (46.99%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -1,23 +1,31 @@
+// /**
+//  * @param {number} x
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var myPow = function(x, n) {
+//     if (n === 0) return 1;
+//     if (n < 0) {
+//         x = 1 / x;
+//         n = -n;
+//     }
+
+//     if (n % 2 === 0) { //even case
+//         const temp = myPow(x, n / 2);
+//         return temp * temp;
+//     } else { //odd case
+//         return x * myPow(x, n - 1);
+//     }
+// };
+
 /**
  * @param {number} x
  * @param {number} n
  * @return {number}
  */
 var myPow = function(x, n) {
-    if (n === 0) return 1;
-    if (n < 0) {
-        x = 1 / x;
-        n = -n;
-    }
-
-    if (n % 2 === 0) { //even case
-        const temp = myPow(x, n / 2);
-        return temp * temp;
-    } else { //odd case
-        return x * myPow(x, n - 1);
-    }
+    return x**n
 };
-
 
 // var myPow = function(x, n) {
 
