95eb603ce2f33776b930cea64b4e64c8bb1da3c6
Kevin Cho <126820348+kcho760@users.noreply.github.com>
2024-04-18 23:30:58 -0400

    Time: 75 ms (53.07%), Space: 52.7 MB (56.86%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+const CONFIG = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+var numIslands = function(grid) {
+    let output = 0;
+
+    const checkIsInValid = (row, col) => row < 0 || row >= grid.length || col < 0 || col >= grid[0].length;
+
+    const dfs = (row, col, grid,) => {
+        if (checkIsInValid(row, col) || grid[row][col] === '0') return;
+
+        grid[row][col] = "0";
+
+        for (const item of CONFIG) {
+            dfs(row + item[0], col + item[1], grid);
+        }
+    }
+
+    for (let i = 0 ; i < grid.length; i++) {
+        for (let j = 0 ; j < grid[0].length; j++) {
+            if (grid[i][j] === "1") {
+                output++;
+                dfs(i, j, grid);
+            }
+        }
+    }
+
+    return output;
+};
