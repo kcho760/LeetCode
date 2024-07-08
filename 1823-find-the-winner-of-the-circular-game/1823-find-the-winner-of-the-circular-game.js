64f9f7b9868aa75f72d2b0a4725498eb69778325
Kevin Cho <126820348+kcho760@users.noreply.github.com>
2024-07-08 09:44:12 -0400

    Time: 56 ms (67.59%), Space: 51.1 MB (43.45%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findTheWinner = function(n, k) {
+    const array = Array.from({ length: n }, (_, index) => index + 1);
+    var i=0;
+    while (array.length > 1) {
+        i = (i + k - 1) % array.length;
+        array.splice(i, 1);
+    }
+    return array[0]
+};
